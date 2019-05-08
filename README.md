19370bf9a7b132e30740691d3c76057ce0447420
Andres Michel <andresmichelg@gmail.com>
2019-05-08 10:58:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-native-list-row-ui
+
+## Getting Started
+
+- [Installation](#installation)
+
+### Installation
+
+```bash
+$ npm install react-native-list-row-ui
+```
---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native'
+
+export default class ListRow extends React.Component {
+  render() {
+    const { color, labelColor } = this.props
+    return (
+      <View style={styles.container}>
+        <View style={styles.infoContainer}>
+          <View style={styles.headerContainer}>
+            <Text numberOfLines={1} style={[styles.title, { color }]}>{this.props.title}</Text>
+            <Text numberOfLines={1} style={[styles.label, { color }]}>{this.props.label}</Text>
+          </View>
+          <Text numberOfLines={1} style={[styles.description, { color: labelColor }]}>{this.props.description}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+ListRow.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  labelColor: PropTypes.string,
+}
+
+ListRow.defaultProps = {
+  color: '#000',
+  labelColor: '#a1a1a1',
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 80,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  infoContainer: {
+    flex: 1,
+    marginLeft: 15,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  title: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginRight: 15,
+  },
+  description: {
+    fontSize: 16,
+  },
+  label: {
+    fontSize: 14,
+    marginLeft: 'auto'
+  },
+})
